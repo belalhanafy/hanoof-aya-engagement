e9ac3bee7c11e28b464f6da93fbdbdefe3240db8
belalhanafy <belalhanafy373@gmail.com>
2025-12-03 14:34:38 +0200

    vercel problem

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    historyApiFallback: true,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
